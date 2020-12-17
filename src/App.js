91144837c702ea45b9fa4d0fdb15379c1bde7ad2
AuuuNya <ma1ive@qq.com>
2020-12-17 19:19:47 +0800

    update

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import './App.css';
+import './block.css';
 
-function UserName(props){
-    return (
-        <div>
-            Hello, { props.name }
-        </div>
-    )
-}
-const arrMap = [
-    {
-        id:1,
-        name:"AuuuNya",
-        age:25
-    },
-    {
-        id:2,
-        name:"TuuuNya",
-        age:23
-    }
-]
+const DEFAULT_QUERY = 'redux';
+const DEFAULT_HPP = '100';
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
+
+
+const largeColumn = {
+    width: '40%',
+};
+
+const midColumn = {
+    width: '30%',
+};
+
+const smallColumn = {
+    width: '10%',
+};
 function Button(props){
-    console.log("button", props)
     const { onClick, className = '', children } = props;
     return (
         <button onClick={onClick} className={className} type="button">
@@ -29,68 +30,134 @@ function Button(props){
         </button>
     )
 }
+
 function Search(props){
-    const { onSearchChange, searchValue, children } = props
+    const { onSearchChange, onSearchSubmit, searchTerm, children } = props
     return (
         <form>
-            { children }<input type="text" 
+            <input type="text" 
                 onChange={onSearchChange}
-                value={searchValue}
+                value={searchTerm}
             />
+            <button type="submit" onClick={onSearchSubmit}>{ children }</button>
         </form>
     )
 }
 
-const Table = ({ arrMap, isSearch, searchValue, deleteHandle }) => 
-    <div>
-        {arrMap.filter(isSearch(searchValue)).map(item => 
-            <div key={item.id}>
-                <span>{item.id}</span>
-                <span>name:{item.name}</span>
-                <span>age:{item.age}</span>
-                <span><Button onClick={() => deleteHandle(item.id)}>Delete</Button></span>
-            </div>
-        )}
-    </div>
+function Table(props){
+    const { list, onDismiss } = props
+    return(
+        <div className="table">
+            {list.map(item => 
+                <div key={item.objectID} className="table-row">
+                    <span style={largeColumn}><a href={item.url}>{item.title}</a></span>
+                    <span style={smallColumn}>{item.points}</span>
+                    <span style={smallColumn}>{item.author}</span>
+                    <span style={midColumn}>{item.created_at}</span>
+                    <span style={smallColumn}><Button className="button-inline" onClick={() => onDismiss(item.objectID)}>Delete</Button></span>
+                </div>
+            )}
+        </div>
+    )
+}
 class App extends React.Component {
     constructor(props){
         super(props)
         this.state = {
-            name: "AuuuNya",
-            arrMap,
-            searchValue: "",
+            results: null,
+            searchKey: '',
+            result: null,
+            searchTerm: DEFAULT_QUERY,
+            error: null,
         }
-        this.deleteHandle = this.deleteHandle.bind(this)
+        this.setSearchTopStories = this.setSearchTopStories.bind(this)
+        this.fetchSearchTopStories = this.fetchSearchTopStories.bind(this)
         this.onSearchChange = this.onSearchChange.bind(this)
+        this.onDismiss = this.onDismiss.bind(this)
+        this.onSearchSubmit = this.onSearchSubmit.bind(this)
+        this.needsToSearchTopStories = this.needsToSearchTopStories.bind(this)
     }
-
-    isSearch(searchItem){
-        return function(item){
-            return item.name.toLowerCase().includes(searchItem.toLowerCase())
+    needsToSearchTopStories(searchTerm){
+        return !this.state.results[searchTerm]
+    }
+    componentDidMount(){
+        const { searchTerm } = this.state;
+        this.setState({searchKey: searchTerm})
+        this.fetchSearchTopStories(searchTerm)
+    }
+    setSearchTopStories(result) {
+        console.log("result:", result)
+        const { hits, page } = result
+        const { searchKey, results } = this.state
+        const oldHits = results && results[searchKey] ? results[searchKey].hits : [];
+        const updateHits = [
+            ...oldHits, ...hits
+        ]
+        this.setState({
+            results: {
+                ...results,
+                [searchKey]: {hits: updateHits, page}
+            }
+        })
+    }
+    fetchSearchTopStories(searchTerm, page=0){
+        console.log("url:", `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
+        fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}&${PARAM_HPP}${DEFAULT_HPP}`)
+        .then(response => response.json())
+        .then(result => this.setSearchTopStories(result))
+        .catch(e => this.setState({error: e}));
+    }
+    onSearchChange(text){
+        this.setState({
+            searchTerm: text.target.value
+        })
+    }
+    onSearchSubmit(e){
+        const { searchTerm } = this.state
+        this.setState({searchKey: searchTerm})
+        if (this.needsToSearchTopStories(searchTerm)){
+            this.fetchSearchTopStories(searchTerm);
         }
+        e.preventDefault();
     }
-    // const isSearch = searchItem => item => item.name.toLowerCase().includes(searchItem.toLowerCase());
-    deleteHandle(id){
-        const newArr = this.state.arrMap.filter((item)=> {
-            return item.id !== id
-        })
+    onDismiss(id){
+        // const isNotId = item => item.objectID !== id;
+        // const updateHits = this.state.result.hits.filter(isNotId)
+        // hits 覆盖掉了 state.result 里面的hits
+        const { searchKey, results } = this.state;
+        const { hits, page } = results[searchKey];
+        const updateHits = {hits: this.state.result.hits.filter(item => {
+            return item.objectID !== id
+        })}
         this.setState({
-            arrMap: newArr
-        })
-        console.log("111",this.state.arrMap)
-    }
-
-    onSearchChange(event){
-        this.setState({
-            searchValue: event.target.value
+            // { ...this.state.result, updateHits} 可替换
+            results: {
+                ...results,
+                [searchKey]: {hits: updateHits, page}
+            }
         })
     }
     render(){
+      const { searchTerm, results, searchKey, error }  = this.state
+      const page = (results && results[searchKey] && results[searchKey].page) || 0;
+      const list = (results && results[searchKey] && results[searchKey].hits) || [];
       return (
-        <div className="App">
-            <UserName name={this.state.name}/>
-            <Search onSearchChange={this.onSearchChange} searchValue={this.state.searchValue}>Search</Search>
-            <Table  arrMap={this.state.arrMap} isSearch={this.isSearch} searchValue={this.state.searchValue} deleteHandle={this.deleteHandle} />
+        <div className="page">
+            { error
+            ? <div className="interactions">
+                <p>Something went wrong.</p>
+            </div>
+            : <div className="interactions">
+                <Search onSearchChange={this.onSearchChange} searchTerm={this.state.searchTerm} onSearchSubmit={this.onSearchSubmit}>Search</Search>
+                <Table 
+                    list={list}
+                    onDismiss={this.onDismiss}
+                />
+                <div className="interactions">
+                    <Button onClick={() => this.fetchSearchTopStories(searchKey, page + 1)}>MORE</Button>
+                </div>
+            </div>
+            }
         </div>
       )
     } 
